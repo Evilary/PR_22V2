d9883bb4c9f49979799cdc631ec564c901af457d
df275 <df275@EVIL>
2026-04-05 03:43:11 +0500

    Разработанный контроллер предметов. Чернышков.

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop_Chernyshkov_Final.Data.Interfaces;
+
+namespace Shop_Chernyshkov_Final.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private IItems IAllItems;
+        private ICategorys IAllCategorys;
+
+        public ItemsController(IItems IAllItems, ICategorys IAllCategorys)
+        {
+            this.IAllItems = IAllItems;
+            this.IAllCategorys = IAllCategorys;
+        }
+        public ViewResult List()
+        {
+            ViewBag.Title = "Страница с предметами";
+
+            var cars = IAllItems.AllItems;
+            return View(cars);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Shop_Chernyshkov_Final.Data.Interfaces;
+using Shop_Chernyshkov_Final.Data.Mocks;
 
 namespace Shop_Chernyshkov_Final
 {
@@ -23,6 +19,11 @@ namespace Shop_Chernyshkov_Final
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICategorys, MockCategorys>();
+            services.AddTransient<IItems, MockItems>();
+
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+
             services.AddMvc();
         }
 
