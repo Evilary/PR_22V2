d5482edea879aec0c3d4a75aee7ccd35f75b3e8a
df275 <df275@EVIL>
2026-04-05 02:58:42 +0500

    Разработанные модели данных. Чернышков.

---
+++ Data/Models/Categoriescs.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Shop_Chernyshkov_Final.Data.Models
+{
+    public class Categoriescs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Items> Items { get; set; }
+    }
+}
---
+++ Data/Models/Items.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop_Chernyshkov_Final.Data.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string Img { get; set; }
+        public int Price { get; set; }
+
+        public Categorys Categorys { get; set; }
+    }
+}
