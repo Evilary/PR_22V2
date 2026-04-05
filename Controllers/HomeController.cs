909005d52889d6ac6a6abf655d0b47a94c5372c9
df275 <df275@EVIL>
2026-04-05 22:53:25 +0500

    Разработанное перенаправление на страницу с предметами. Чернышков.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Shop_Chernyshkov_Final.Controllers
+
+{
+    public class HomeController : Controller
+    {
+        public RedirectResult Index()
+        {
+            return Redirect("/Items/List");
+        }
+    }
+}
