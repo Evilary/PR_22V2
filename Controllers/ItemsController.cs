961a0c8b0de74b14d7b634340e61eddaabc30d0d
df275 <df275@EVIL>
2026-04-05 19:59:14 +0500

    Метод добавления предмета. Чернышков

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Shop_Chernyshkov_Final.Data.Interfaces;
 using Shop_Chernyshkov_Final.Data.Models;
@@ -14,10 +18,13 @@ namespace Shop_Chernyshkov_Final.Controllers
 
         private VMItems VMItems = new VMItems();
 
-        public ItemsController(IItems IAllItems, ICategorys IAllCategorys)
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public ItemsController(IItems IAllItems, ICategorys IAllCategorys, IHostingEnvironment hostingEnvironment)
         {
             this.IAllItems = IAllItems;
             this.IAllCategorys = IAllCategorys;
+            this.hostingEnvironment = hostingEnvironment;
         }
         public ViewResult List(int id = 0)
         {
@@ -34,6 +41,27 @@ namespace Shop_Chernyshkov_Final.Controllers
         {
             IEnumerable<Categorys> categorys = IAllCategorys.AllCategorys;
             return View(categorys);
+        }
+        [HttpPost]
+        public RedirectResult Add(string name, string description, IFormFile files, float price, int idCategory)
+        {
+            if(files != null)
+            {
+                var uploads = Path.Combine(hostingEnvironment.WebRootPath, "img");
+                var filePath = Path.Combine(uploads, files.FileName);
+                files.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+
+            Items newItems = new Items();
+            newItems.Name = name;
+            newItems.Description = description;
+            newItems.Img = files.FileName;
+            newItems.Price = Convert.ToInt32(price);
+            newItems.Category = new Categorys() { Id = idCategory };    
+
+            int id = IAllItems.Add(newItems);
+            return Redirect("/Items/Update?id=" + id);
+
         }
     }
 }
